be5d5565c048fef98475887cb15143c1d2fbab89
AlexFariaa <alexcaras1@hotmail.com>
2022-01-19 16:14:10 -0300

    terceiro commit

--- extra6a1.js
+++ extra6a1.js
@@ -1,18 +1,19 @@
 console.clear();
-const pc = require('prompt-sync')();
+var pc = require('prompt-sync')();
 
 let vd
 let atk
 
 vd = +pc("Digite a quantidade de vida de um monstro (de 10 a 50): ");
-console.log("A vida do monstro é de: ${vd} pontos");
+console.log(`A vida do monstro é de: ${vd} pontos`);
 console.log();
 atk = +pc("Digite agora o dano de ataque por turno do seu guerreiro: ");
-console.log("Seu dano de ataque é de: ${atk}/turno");
+console.log(`Seu dano de ataque é de: ${atk}/turno`);
 console.log();
 console.log("QUE OS JOGOS COMECEM");
 console.log();
-console.log(`Você irá derrotar o monstro em ${(vd/atk)} turnos`);
+console.log(`Você irá derrotar o monstro em ${vd/atk} turnos`);
+console.log();
 
 
 
